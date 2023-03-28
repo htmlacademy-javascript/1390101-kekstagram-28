25ba7b675649b35e5ca545f32a373ca847b5b071
Kris-js <devilmiss@mail.ru>
2023-03-28 22:52:36 +0300

    Добавляет валидацию формы

--- js/form.js
+++ js/form.js
@@ -1,10 +1,15 @@
 import { isEscapeKey } from './util.js';
 
+const TAG_ERROR_TEXT = 'Неправильно заполнены хештеги';
+
+const form = document.querySelector('.img-upload__form');
 const modalOverlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 const uploadFile = document.querySelector ('.img-upload__input');
 const cancelButton = document.querySelector('.img-upload__cancel');
 const commentField = document.querySelector('.text__description');
+const textHashtags = document.querySelector('.text__hashtags');
+
 
 const onShowModal = () => {
   modalOverlay.classList.remove('hidden');
@@ -35,3 +40,25 @@ commentField.addEventListener('focus', () => {
 commentField.addEventListener('blur', () => {
   document.addEventListener('keydown', onDocumentKeydown);
 });
+
+textHashtags.addEventListener('focus', () => {
+  document.removeEventListener('keydown', onDocumentKeydown);
+});
+
+textHashtags.addEventListener('blur', () => {
+  document.addEventListener('keydown', onDocumentKeydown);
+});
+
+const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__form-wrapper',
+  errorTextParent: 'img-upload__form-element',
+  errorTextClass: 'img-upload__form-error'
+});
+
+pristine.addValidator(
+  textHashtags,
+  hashtag,
+  TAG_ERROR_TEXT
+);
