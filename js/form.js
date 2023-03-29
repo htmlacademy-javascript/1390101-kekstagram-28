f6a8488875d5e3ea50b287535954b80317713f5b
Kris-js <devilmiss@mail.ru>
2023-03-29 11:09:50 +0300

    Добавляет валидирование

--- js/form.js
+++ js/form.js
@@ -1,6 +1,7 @@
 import { isEscapeKey } from './util.js';
 
 const TAG_ERROR_TEXT = 'Неправильно заполнены хештеги';
+const MAX_HESHTEGS_COUNT = 5;
 
 const form = document.querySelector('.img-upload__form');
 const modalOverlay = document.querySelector('.img-upload__overlay');
@@ -10,6 +11,11 @@ const cancelButton = document.querySelector('.img-upload__cancel');
 const commentField = document.querySelector('.text__description');
 const textHashtags = document.querySelector('.text__hashtags');
 
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__form-wrapper',
+  errorTextParent: 'img-upload__form-element',
+  errorTextClass: 'img-upload__form-error'
+});
 
 const onShowModal = () => {
   modalOverlay.classList.remove('hidden');
@@ -21,6 +27,8 @@ const onCloseModal = () => {
   modalOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
+  form.reset();
+  pristine.reset();
 };
 
 function onDocumentKeydown(evt) {
@@ -30,8 +38,10 @@ function onDocumentKeydown(evt) {
   }
 }
 
-uploadFile.addEventListener('change', onShowModal);
-cancelButton.addEventListener('click', onCloseModal);
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
 
 commentField.addEventListener('focus', () => {
   document.removeEventListener('keydown', onDocumentKeydown);
@@ -50,15 +60,30 @@ textHashtags.addEventListener('blur', () => {
 });
 
 const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
+const isValidTegs = (tag) => hashtag.test(tag);
+const hashValidCount = (tags) => tags.length <= MAX_HESHTEGS_COUNT;
 
-const pristine = new Pristine(form, {
-  classTo: 'img-upload__form-wrapper',
-  errorTextParent: 'img-upload__form-element',
-  errorTextClass: 'img-upload__form-error'
-});
+const hashUniqueTags = (tags) => {
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+
+  return lowerCaseTags.length === new Set (lowerCaseTags).size;
+};
+
+const validateTags = (value) => {
+  const tags = value
+    .trim()
+    .split(' ')
+    .filter((tag) => tag.trim().length);
+
+  return hashValidCount(tags) && hashUniqueTags(tags) && tags.every(isValidTegs);
+};
 
 pristine.addValidator(
   textHashtags,
-  hashtag,
+  validateTags,
   TAG_ERROR_TEXT
 );
+
+uploadFile.addEventListener('change', onShowModal);
+cancelButton.addEventListener('click', onCloseModal);
+form.addEventListener('submit', onFormSubmit);
