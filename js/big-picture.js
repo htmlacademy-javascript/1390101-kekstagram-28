abd130fef9d5259e1219cf196d25b02ec21e9849
Kris-js <devilmiss@mail.ru>
2023-04-12 23:44:34 +0300

    добавляет отрисовку комментов

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,5 +1,6 @@
 import { isEscapeKey } from './util.js';
 
+const COMMENTS_STEP = 5;
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
 const bigPictureImg = document.querySelector('.big-picture__img img');
@@ -10,6 +11,9 @@ const socialComments = document.querySelector('.social__comments');
 const socialComment = document.querySelector('.social__comment');
 const socialCommentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
+
+let currentCount = COMMENTS_STEP;
+let currentsCount = [];
 
 const closeBigPicture = () => {
   document.body.classList.remove('modal-open');
@@ -29,8 +33,22 @@ function onDocumentKeydown(evt) {
   }
 }
 
-const renderComments = (comments) => {
+const renderComments = () => {
   socialComments.innerHTML = '';
+
+  currentCount = (currentCount > currentsCount.length) ? currentsCount.length : currentCount;
+
+  const comments = currentsCount.slice(0, currentCount);
+
+  if (currentsCount.length <= COMMENTS_STEP || currentCount >= currentsCount.length) {
+    commentsLoader.classList.add('hidden');
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  socialCommentCount.textContent = `${currentCount} из ${currentsCount.length} комментариев`;
+
+  const commentFragment = document.createDocumentFragment();
 
   comments.forEach((comment) => {
     const commentCopy = socialComment.cloneNode(true);
@@ -40,8 +58,15 @@ const renderComments = (comments) => {
     newPhoto.alt = comment.name;
     newComment.textContent = comment.message;
 
-    socialComments.appendChild(commentCopy);
+    commentFragment.appendChild(commentCopy);
   });
+
+  socialComments.appendChild(commentFragment);
+};
+
+const onLoadCommentsButtonClick = () => {
+  currentCount += COMMENTS_STEP;
+  renderComments();
 };
 
 const renderBigPicture = (photo) => {
@@ -58,7 +83,11 @@ const showBigPicture = (photo) => {
   renderBigPicture(photo);
   socialCommentCount.classList.add('hidden');
   commentsLoader.classList.add('hidden');
+  renderComments();
 
+  currentsCount = comments.slice();
+
+  commentsLoader.addEventListener('click', onLoadCommentsButtonClick);
   bigPictureCancel.addEventListener('click', onCloseButtonClick);
   document.addEventListener('keydown', onDocumentKeydown);
 };
