fb47ee35655226bb662e6a05b45e6269f420cda0
Kris-js <devilmiss@mail.ru>
2023-04-14 17:02:46 +0300

    исправляет по замечаниям

--- js/big-picture.js
+++ js/big-picture.js
@@ -18,7 +18,9 @@ let currentsCount = [];
 const closeBigPicture = () => {
   document.body.classList.remove('modal-open');
   bigPicture.classList.add('hidden');
+  currentCount = COMMENTS_STEP;
 
+  commentsLoader.removeEventListener('click', onLoadCommentsButtonClick);
   bigPictureCancel.removeEventListener('click', onCloseButtonClick);
   document.removeEventListener('keydown', onDocumentKeydown);
 };
@@ -36,7 +38,7 @@ function onDocumentKeydown(evt) {
 const renderComments = () => {
   socialComments.innerHTML = '';
 
-  currentCount = (currentCount > currentsCount.length) ? currentsCount.length : currentCount;
+  currentCount = Math.min(currentCount, currentsCount.length);
 
   const comments = currentsCount.slice(0, currentCount);
 
@@ -46,7 +48,8 @@ const renderComments = () => {
     commentsLoader.classList.remove('hidden');
   }
 
-  socialCommentCount.textContent = `${currentCount} из ${currentsCount.length} комментариев`;
+  socialCommentCount.innerHTML = `${currentCount} из <span class="comments-count">
+  ${currentsCount.length}</span> комментариев`;
 
   const commentFragment = document.createDocumentFragment();
 
@@ -64,28 +67,24 @@ const renderComments = () => {
   socialComments.appendChild(commentFragment);
 };
 
-const onLoadCommentsButtonClick = () => {
+function onLoadCommentsButtonClick() {
   currentCount += COMMENTS_STEP;
   renderComments();
-};
+}
 
 const renderBigPicture = (photo) => {
   bigPictureImg.src = photo.url;
   likesCount.textContent = photo.likes;
   commentsCount.textContent = photo.comments.length;
   socialCaption.textContent = photo.description;
-  renderComments(photo.comments);
 };
 
 const showBigPicture = (photo) => {
   document.body.classList.add('modal-open');
   bigPicture.classList.remove('hidden');
   renderBigPicture(photo);
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+  currentsCount = photo.comments;
   renderComments();
-
-  currentsCount = comments.slice();
 
   commentsLoader.addEventListener('click', onLoadCommentsButtonClick);
   bigPictureCancel.addEventListener('click', onCloseButtonClick);
